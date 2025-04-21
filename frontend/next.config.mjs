21f3811cd0ae38e2375d2efd2b76ee8948bc0408
Pgas <shantaram.ff@gmail.com>
2025-04-21 16:32:13 -0400

    denuevo

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/pgapps/boletas',
   output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 }
-
 export default nextConfig
